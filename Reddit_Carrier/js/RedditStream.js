6433a2473b30ab59f8a31f4e615c1ad15c5653d4
CowboyCoder <chase.rogers.atsid@gmail.com>
2021-07-07 10:43:21 -0700

    update RedditStream.js

--- Reddit_Carrier/js/RedditStream.js
+++ Reddit_Carrier/js/RedditStream.js
@@ -5,7 +5,7 @@ function timeConverter(UNIX_timestamp){
 	var month = months[a.getMonth()];
 	var date = a.getDate();
 	var hour = a.getHours();
-	var min = a.getMinutes();
+	var min = a.getMinutes(); 
 	var sec = a.getSeconds();
 	var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
 	return time;
